2a2031282dc1c42390af83eaad8790b1b2809515
Malte Bär <maltebaer@posteo.de>
2024-01-22 21:40:31 +0100

    adds style guide by bryan braun

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,30 +1,18 @@
 module.exports = {
-  top: [
-    {
-      text: 'About',
-      url: '/about/'
-    },
-    {
-      text: 'Get started',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
-      text: 'Blog',
-      url: '/blog/'
-    }
-  ],
-  bottom: [
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    }
-  ]
+    top: [
+        {
+            text: 'Style Guide',
+            url: '/style-guide/'
+        }
+    ],
+    bottom: [
+        {
+            text: 'Imprint',
+            url: '/imprint/'
+        },
+        {
+            text: 'Privacy',
+            url: '/privacy/'
+        }
+    ]
 };
